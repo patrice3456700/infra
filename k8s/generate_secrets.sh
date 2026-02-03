441464349bde27f032b3cac565209f5bdc627091
patrice <patrice@christien.fr>
2026-02-03 21:06:29 +0100

    add to git

---
+++ k8s/generate_secrets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Nom du secret Kubernetes
+SECRET_NAME="jwt-secret"
+
+# Génération de la paire de clés RSA
+openssl genpkey -algorithm RSA -out private_key.pem
+openssl rsa -pubout -in private_key.pem -out public_key.pem
+
+# Création du secret Kubernetes avec les clés générées
+kubectl create secret generic $SECRET_NAME \
+  --from-file=private_key.pem \
+  --from-file=public_key.pem 
+
+# Suppression des clés du disque local pour des raisons de sécurité
+rm -f private_key.pem public_key.pem
+
+echo "Secret Kubernetes $SECRET_NAME créé avec succès !"
+
+kubectl create secret generic gcs-credentials \
+  --from-literal=placeholder=vide \
+  -n <le-namespace-de-ton-mongodb>
+
+MountVolume.SetUp failed for volume "gcs-credentials" : secret "gcs-credentials" not found
